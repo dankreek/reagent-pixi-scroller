c52af072f532ae1cf45ef49d3228f1cbe3c2d6d4
Justin May <may.justin@gmail.com>
2016-11-19 20:56:23 -0800

    Add stage

--- src/reagent_pixi_scroller/core.cljs
+++ src/reagent_pixi_scroller/core.cljs
@@ -4,11 +4,21 @@
 
 (enable-console-print!)
 
+(def width 636)
+(def height 480)
+
 (def Stage (r/adapt-react-class js/ReactPIXI.Stage))
 (def Container (r/adapt-react-class js/ReactPIXI.DisplayObjectContainer))
 (def TilingSprite (r/adapt-react-class js/ReactPIXI.TilingSprite))
 
 (defonce app-state (atom {:text "Hello world!"}))
+
+(defn scroller-stage []
+  [Stage {:width width
+          :height height}])
+
+(r/render-component [scroller-stage]
+                    (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
